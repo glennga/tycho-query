79df3cbc117d880f880f3024caee5d2f26c9d681
Glenn <glennjgalvizo@gmail.com>
2018-05-07 18:29:41 -1000

    Just noticed that my queries don't use the InRegion column. This has been removed.

---
+++ submission/load/neo4j/load_supp.py
@@ -0,0 +1,81 @@
+""" Neo4J database loader for the Tycho-2 supplement dataset. Each node has the following properties:
+
+    1. TYC2      <--- GSC Running Number
+    2. TYC3      <--- GSC Component Identifier
+    3. RAmdeg    <--- Mean Right Ascension
+    4. DEmdeg    <--- Mean Declination
+    5. BTmag    <--- Magnitude
+
+Each region (as of now) has the following property:
+
+    1. TYC1      <--- GSC Region Number
+
+Usage: python3 load_supp.py [uri] [username] [password] [catalog]
+"""
+from neo4j.v1 import GraphDatabase
+from sys import argv
+
+# Chunk size of each transaction.
+CHUNK_SIZE = 10000
+
+if __name__ == '__main__':
+    # We need to be passed the URI, username, password, and location of the catalog.
+    if len(argv) != 5:
+        print('Usage: python3 load_supp.py [uri] [username] [password] [catalog]')
+        exit(1)
+
+    # Connect to the database, and start a session.
+    driver = GraphDatabase.driver(argv[1], auth=(argv[2], argv[3]))
+    session = driver.session()
+
+    # Create our indices.
+    session.run('CREATE INDEX ON :Star(TYC1, TYC2, TYC3)')
+    session.run('CREATE INDEX ON :Region(TYC1)')
+
+    # Commit counter, and the start of the first transaction.
+    tx = session.begin_transaction()
+    commit_i = 0
+
+    # Load our file. We are going to read line by line (ughghghghghghhggh).
+    with open(argv[4], 'r') as c_f:
+        for entry in c_f:
+            try:
+                if commit_i >= CHUNK_SIZE:
+                    tx.commit()
+                    tx, commit_i = session.begin_transaction(), 0
+
+                node = {
+                    'TYC1': int(entry[:4]),
+                    'TYC2': int(entry[6:10]),
+                    'TYC3': int(entry[11]),
+                    'RAmdeg': float(entry[15:27]),
+                    'DEmdeg': float(entry[28:40]),
+                    'BTmag': float(entry[83:89]),
+                }
+
+                tx.run("""
+                    CREATE (s:Star {
+                        TYC1: {TYC1}, 
+                        TYC2: {TYC2},
+                        TYC3: {TYC3},
+                        RAmdeg: {RAmdeg},
+                        DEmdeg: {DEmdeg},
+                        BTmag: {BTmag}
+                    })""", node)
+                tx.run("""
+                    MERGE (a:Region {
+                        TYC1: {TYC1}
+                    })""", node)
+                tx.run("""
+                    MATCH (s:Star), (a:Region) WHERE
+                        s.TYC1 = {TYC1} AND
+                        s.TYC2 = {TYC2} AND
+                        s.TYC3 = {TYC3} AND
+                        a.TYC1 = {TYC1}
+                    CREATE (a)-[:CONTAINS]->(s)""", node)
+                commit_i = commit_i + 1
+
+            except ValueError as e:
+                pass
+
+    tx.commit(), session.close(), driver.close()
---
+++ submission/run/cassandra/query.py
@@ -0,0 +1,105 @@
+""" Cassandra query executor (and timer). CQL files must contain valid syntax, queries must be separated by a
+ newline, and the number of queries per set must be specified.
+
+Usage: python3 query.py [uri] [cql-file] [index-file] [n]
+"""
+
+from cassandra.query import tuple_factory
+from cassandra.cluster import Cluster
+from numpy import average, std
+from timeit import timeit
+from time import sleep
+from sys import argv
+
+
+def lookup_tyc(ra, dec, index_file):
+    """ Given some point in space, search the index file for the matching GSC region.
+
+    :param ra: Right ascension to search for.
+    :param dec: Declination to search for.
+    :param index_file: Index file to search
+    :return: The matching TYC1 number if one exists. Otherwise, -1.
+    """
+    with open(index_file, 'r') as i_f:
+        for i, entry in enumerate(i_f):
+            try:
+                node = {
+                    'RAmin': float(entry[15:21]),
+                    'RAmax': float(entry[22:28]),
+                    'DEmin': float(entry[29:35]),
+                    'DEmax': float(entry[36:42])
+                }
+                if node['RAmin'] < ra < node['RAmax'] and node['DEmin'] < dec < node['DEmax']:
+                    return i
+
+            except ValueError:
+                pass
+
+    # If no matching TYC is found, return -1.
+    return -1
+
+
+def execute_query_sets(q):
+    """ Given a sequence of queries, execute them and feed the result of the former into the latter.
+
+    :param q: Query sequence to execute.
+    :return: None.
+    """
+    previous = []
+
+    for q_i in q:
+        # Custom function (not CQL), compute the hash for the following values.
+        if q_i.split()[0] == 'COMPUTE' and q_i.split()[1] == 'HASH':
+            ra, dec = float(q_i.split()[2]), float(q_i.split()[3])
+            previous = [[lookup_tyc(ra, dec, argv[3])]]
+
+        else:
+            # Otherwise, execute the query. Pass the previous result if desired.
+            if q_i.find('?') != -1:
+                # Note: We execute the query even if the result of 'previous' is none.
+                previous = [x for x in session.execute(
+                    q_i.replace('?', str(previous[0][0]) if len(previous) != 0 else '-1'))]
+
+            else:
+                previous = [x for x in session.execute(q_i)]
+            # print('Result: ' + (','.join([str(x) for x in previous]) if len(previous) != 0 else 'None'))
+
+
+if __name__ == '__main__':
+    # We need to be passed the URI, the location of the query file, and the number of subqueries.
+    if len(argv) != 5:
+        print('Usage: python3 query.py [uri] [cql-file] [index-file] [n]')
+        exit(1)
+
+    # Connect to the database, and start a session.
+    cluster = Cluster([argv[1]])
+    session = cluster.connect()
+    session.set_keyspace('tycho')
+    session.row_factory = tuple_factory
+
+    # Load our file into memory, do not read our comments.
+    queries, q_t, r_t, n_c = [], [], [], 0
+    with open(argv[2], 'r') as cql_f:
+        for line in cql_f:
+
+            # Flush our buffer q_t when we have a complete subquery set.
+            if n_c < int(argv[4]):
+                n_c = (n_c + 1) if line.strip() and not line.startswith(';') else n_c
+                q_t.append(line.strip()) if line.strip() and not line.startswith(';') else None
+            else:
+
+                # Append if necessary.
+                queries.append(q_t)
+                if line.strip() and not line.startswith(';'):
+                    n_c, q_t = 1, [line.strip()]
+                else:
+                    n_c, q_t = 0, []
+
+    # Run our queries 15 times.
+    for i in range(15):
+        for j, q in enumerate(queries):
+            r_t.append(timeit(stmt=lambda: execute_query_sets(q), number=1))
+            print('Running Time [Query {}, Run {}]: {}'.format(j, i, r_t[-1]))
+            sleep(0.25)
+
+    print('Average Running Time: {} +/- {}'.format(average(r_t), std(r_t)))
